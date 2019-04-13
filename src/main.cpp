1310034bb5d6f6bd858f515f0d443187391ee167
Jay Wiggins <jay@irk.com>
2019-04-13 11:10:09 -0700

    working through it

--- src/main.cpp
+++ src/main.cpp
@@ -16,133 +16,285 @@
 
 #include <MovingAverageFloat.h>
 
-Adafruit_MMA8451 mma = Adafruit_MMA8451();
-
-
-// These are the pins used
+// Music Maker Featherwing....
 #define VS1053_RESET -1 // VS1053 reset pin (not used!)
-
 // changed for m0 feather
 #define VS1053_CS 6   // VS1053 chip select pin (output)
 #define VS1053_DCS 10 // VS1053 Data/command select pin (output)
 #define CARDCS 5      // Card chip select pin
 #define VS1053_DREQ 9 // VS1053 Data request, ideally an Interrupt pin
 
+// NeoPixel...
+#define NEOPIX_LED_PIN 8
+#define NEOPIX_LED_COUNT 1
+
+Adafruit_MMA8451 mma = Adafruit_MMA8451();
 Adafruit_VS1053_FilePlayer musicPlayer =
     Adafruit_VS1053_FilePlayer(VS1053_RESET, VS1053_CS, VS1053_DCS, VS1053_DREQ, CARDCS);
-
-
-#define LED_PIN 8
-#define LED_COUNT 1
-Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel strip(NEOPIX_LED_COUNT, NEOPIX_LED_PIN, NEO_GRB + NEO_KHZ800);
 
 // Buffer will be 16 samples long, it will take 16 * sizeof(float) = 64 bytes of RAM
-MovingAverageFloat <16> filter;
+MovingAverageFloat<16> angle;
+MovingAverageFloat<16> speedDown;
 
-void setup() {
+// state machine values...
+enum State_enum
+{
+  NOT_RACING,
+  AT_STARTING_LINE,
+  MOVING,
+  STOPPED,
+  GETTING_PICKED_UP
+};
+
+uint8_t state = NOT_RACING;
+
+void errorMode();
+void startAccelerometer();
+void startNeoPixel();
+void startMusicMaker();
+void startSdCard();
+void setState();
+void runStateMachine();
+void updateAccelerometer();
+bool onTrack();
+bool movingDown();
+bool stopped();
+bool jostling();
+bool still();
+
+#define STARTING_LINE_ARRAY_SIZE 5
+const char *startingLineFiles[] = {"start0.ogg", "start1.ogg", "start2.ogg", "start3.ogg", "start4.ogg", "start5.ogg"};
+uint8_t startingLineFilePosition = 0;
+
+#define MOVING_ARRAY_SIZE 1
+const char *movingFiles[] = {"move0.ogg", "move2.ogg"};
+uint8_t movingFilePosition = 0;
+
+#define STOPPED_ARRAY_SIZE 0
+const char *stoppedFiles[] = {"stop0.ogg"};
+uint8_t stoppedFilePosition = 0;
+
+#define GETTING_PICKED_UP_ARRAY_SIZE 1
+const char *gettingPickedUpFiles[] = {"pick1.ogg", "pick0.ogg"};
+uint8_t gettingPickedUpFilePosition = 0;
+
+void setup()
+{
   Serial.begin(115200);
-  delay(500);
-  Serial.println("\n\nA-Team Van Pinewood Derby code");
+  Serial.println(F("starting up...."));
+  startAccelerometer();
+  startNeoPixel();
+  startSdCard();
+  startMusicMaker();
+}
 
-  delay(3000);
-  if (! mma.begin()) {
+uint8_t racing = false;
+
+bool moving()
+{
+  sensors_event_t event;
+  mma.getEvent(&event);
+  return (false);
+}
+
+uint8_t count = 0;
+uint32_t red = strip.Color(50, 0, 0);
+uint32_t blue = strip.Color(0, 0, 50);
+uint32_t green = strip.Color(0, 50, 0);
+uint32_t orange = strip.Color(50, 20, 0);
+uint32_t purple = strip.Color(20, 0, 20);
+
+unsigned long previousMillis = 0;
+uint16_t checkStateInterval = 200;
+
+void loop()
+{
+  unsigned long currentMillis = millis();
+  updateAccelerometer();
+  if ((unsigned long)(currentMillis - previousMillis) >= checkStateInterval)
+  {
+    setState();
+
+    // state = AT_STARTING_LINE;
+    runStateMachine();
+    previousMillis = currentMillis;
+  }
+}
+
+void errorMode()
+{
+  while (1)
+  {
+    strip.setPixelColor(0, strip.Color(50, 0, 0));
+    strip.show();
+    delay(250);
+    strip.setPixelColor(0, strip.Color(0, 0, 0));
+    strip.show();
+    delay(250);
+  }
+}
+
+void startAccelerometer()
+{
+  if (!mma.begin())
+  {
     Serial.println("Couldnt start");
-    // while (1);
+    errorMode();
   }
   Serial.println("MMA8451 found!");
   mma.setRange(MMA8451_RANGE_2_G);
+}
 
-  if (!musicPlayer.begin()) { // initialise the music player
-    Serial.println(F("Couldn't find VS1053, do you have the right pins defined?"));
-    // while (1); // hang....
-  }
-
-  delay(100);
-  Serial.println(F("VS1053 found"));
-
-  musicPlayer.sineTest(0x44, 500); // Make a tone to indicate VS1053 is working
-
-  if (!SD.begin(CARDCS)) {
-    Serial.println(F("SD failed, or not present"));
-    // while (1); // don't do anything more
-  }
-  Serial.println("SD OK!");
-
+void startNeoPixel()
+{
   strip.begin();
   strip.clear();
-  strip.setPixelColor(0, strip.Color(0, 150, 0));
+  strip.setPixelColor(0, strip.Color(0, 10, 0));
   strip.show();
   delay(1000);
+  strip.clear();
+  strip.show();
+}
 
+void startMusicMaker()
+{
+  if (!musicPlayer.begin())
+  {
+    Serial.println(F("Couldn't find VS1053, do you have the right pins defined?"));
+    errorMode();
+  }
+  musicPlayer.sineTest(0x44, 500);
+  Serial.println(F("VS1053 found"));
   // Set volume for left, right channels. lower numbers == louder volume!
   musicPlayer.setVolume(0, 0);
   musicPlayer.useInterrupt(VS1053_FILEPLAYER_PIN_INT); // DREQ int
-  delay(100);
-  Serial.println(F("Playing full track 001"));
-  musicPlayer.startPlayingFile("2.wav");
-  delay(500);
-
 }
 
-uint8_t countOnTrack = 0;
-#define REQUIRED_COUNTS_ON_TRACK 10
-bool onTrack() {
+void startSdCard()
+{
+  if (!SD.begin(CARDCS))
+  {
+    Serial.println(F("SD failed, or not present"));
+    errorMode();
+  }
+  Serial.println("SD OK!");
+}
+
+void updateAccelerometer()
+{
   sensors_event_t event;
   mma.getEvent(&event);
-  float val = event.acceleration.x;
-  Serial.print("x:"); Serial.println(val);
-  if(((val > 1) | ((val < -1)))) {
-    if(countOnTrack >= REQUIRED_COUNTS_ON_TRACK) {
-      return true;
-    } else {
-       countOnTrack++;
+  angle.add(event.acceleration.y);
+  speedDown.add(event.acceleration.z);
+}
+
+void runStateMachine()
+{
+  if (musicPlayer.stopped())
+  {
+    switch (state)
+    {
+    case AT_STARTING_LINE:
+      Serial.println("AT_STARTING_LINE");
+      startingLineFilePosition++;
+      if (startingLineFilePosition > STARTING_LINE_ARRAY_SIZE)
+        startingLineFilePosition = 0;
+      musicPlayer.startPlayingFile(startingLineFiles[startingLineFilePosition]);
+      break;
+    default:
+      Serial.println("STATE CASE NOT SET!");
     }
-  } else {
+  }
+}
+
+#define REQUIRED_COUNTS_ON_TRACK 5
+uint8_t countOnTrack = 0;
+bool currentlyOnTrack = false;
+
+bool onTrack()
+{
+  float val = angle.get();
+  Serial.print("y:");
+  Serial.println(val);
+  if (((abs(val) > 1) & ((abs(val) < 8)))) // approxomate the angle i expect the track to start with
+  {
+    if (countOnTrack >= REQUIRED_COUNTS_ON_TRACK)
+    {
+      racing = true;
+      return true;
+    }
+    else
+    {
+      countOnTrack++;
+    }
+  }
+  else
+  {
     countOnTrack = 0;
   }
   return false;
 }
 
-bool moving() {
-  sensors_event_t event;
-  mma.getEvent(&event);
-  return(false);
+bool movingDown()
+{
+  return false;
 }
 
-uint8_t count = 0;
-uint32_t red =  strip.Color(50, 0, 0);
-uint32_t blue =  strip.Color(0, 0, 50);
-uint32_t green =  strip.Color(0, 50, 0);
-uint32_t orange = strip.Color(50, 20, 0);
-uint32_t purple = strip.Color(20, 0, 20);
+bool stopped()
+{
+  return false;
+}
 
-unsigned long previousMillis=0;
-uint16_t led_interval = 1000;
-uint16_t position_interval = 800;
+bool jostling()
+{
+  return false;
+}
 
-void loop() {
-  unsigned long currentMillis = millis();
-  if(moving()) {
-    strip.setPixelColor(0, orange);
-    strip.show();
-  } else {
-    if ((unsigned long)(currentMillis - previousMillis) >= position_interval) {
-      if(onTrack()) {
-        strip.setPixelColor(0, green);
-      } else {
-        strip.setPixelColor(0, purple);
+bool still()
+{
+  return false;
+}
+
+void setState()
+{
+  if (racing)
+  {
+    switch (state)
+    {
+    case AT_STARTING_LINE:
+      if (onTrack())
+      {
+        if (movingDown())
+          state = MOVING;
       }
-      strip.show();
-      previousMillis = currentMillis;
+      else
+      {
+        racing = false;
+        state = NOT_RACING;
+      }
+      break;
+    case MOVING:
+      if (stopped())
+        state = STOPPED;
+      break;
+    case STOPPED:
+      if (jostling())
+        state = GETTING_PICKED_UP;
+      break;
+    case GETTING_PICKED_UP:
+      if (still())
+        state = NOT_RACING;
+      racing = false;
+      break;
     }
-    // if ((unsigned long)(currentMillis - previousMillis) >= led_interval) {
-    //   if(strip.getPixelColor(0) == red) {
-    //     strip.setPixelColor(0, blue);
-    //   } else {
-    //     strip.setPixelColor(0, red);
-    //   }
-    //   strip.show();
-    //   previousMillis = currentMillis;
-    // }
+  }
+  else
+  {
+    if (onTrack())
+    {
+      state = AT_STARTING_LINE;
+      racing = true;
+    }
   }
 }
